b8e0dc6580b782900db42a3b40a312bea7d69156
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-09-29 11:23:14 +0300

    add dockerfile to create environment for docx to pdf conversion

--- Backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970e80ff5b0f6fd54cc4f9c2365e561502992f2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57aba0c3eb1b8c9793622fb58b5b3c442b028210")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
