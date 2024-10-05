d9172715746aeb102aa4acef50be28dcf89fa32b
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-10-05 14:34:45 +0300

    refactoring: create a TemplateItem subcomponent for each template, change styles

---
+++ Frontend/components/TemplateItem/TemplateItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    @mouseover="isHighlighted = true"
+    @mouseleave="isHighlighted = false"
+    :class="[
+      'p-4 border rounded-lg cursor-pointer transition flex items-center justify-between',
+      isHighlighted
+        ? 'bg-gray-200 border-purple-500'
+        : 'bg-white border-gray-300',
+    ]"
+  >
+    <span>{{ template.name }}</span>
+    <button
+      class="px-6 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition"
+      @click="generateLink(template.id)"
+    >
+      Generate link
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { useApi } from "~/composables/useApi";
+
+const api = useApi();
+const props = defineProps({
+  template: {
+    name: String,
+    id: Number,
+  },
+});
+
+const generateLink = async (id) => {
+  /*const link = await api.customFetch(`/contracts/${id}/url`, {});*/
+};
+</script>
