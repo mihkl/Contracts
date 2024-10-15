3068d4d424f77f148573dbf61ddff2652728de39
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-10-15 21:02:19 +0300

    Show form with dynamic values when opening /contracts/{id} page

--- Frontend/pages/contracts/[id].vue
+++ Frontend/pages/contracts/[id].vue
@@ -1,12 +1,40 @@
-<template></template>
+<template>
+  <UForm class="space-y-4">
+    <UFormGroup
+      v-for="(field, index) in contractFields?.fields"
+      :label="field.name"
+      name="contractName"
+      required
+      :key="index"
+    >
+      <UInput type="text" />
+    </UFormGroup>
+  </UForm>
+</template>
+
 <script setup lang="ts">
 import { useRoute } from "nuxt/app";
 
 const route = useRoute();
+const api = useApi();
 
+const contractFields = ref<{
+  fields: { name: string; type: string }[];
+}>();
+
+const id = route.params?.id;
 const signature = route.query?.signature;
 const validFrom = route.query?.validFrom;
 const validUntil = route.query?.validUntil;
 
-console.log(signature, validFrom, validUntil);
+const fields = await api.customFetch<{
+  fields: { name: string; type: string }[];
+}>(
+  `/contracts/${id}?signature=${signature}&validFrom=${validFrom}&validUntil=${validUntil}`,
+  {
+    method: "GET",
+  }
+);
+
+contractFields.value = fields;
 </script>
