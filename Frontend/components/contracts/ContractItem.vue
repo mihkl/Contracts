f40828d8b67bdaa7d410dcb3797a58954ec97d5f
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-10-31 19:52:39 +0200

    add contract progress roadmap svg

--- Frontend/components/contracts/ContractItem.vue
+++ Frontend/components/contracts/ContractItem.vue
@@ -1,25 +1,32 @@
 <template>
   <div
     :class="[
-      'p-4 border rounded-lg cursor-pointer transition flex items-center justify-between bg-white border-gray-300',
+      'p-4 border rounded-lg cursor-pointer transition bg-white border-gray-300',
     ]"
   >
-    <span>{{ contract?.name }}</span>
-    <div class="flex space-x-2">
-      <button
-        class="px-6 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition"
-        @click="openDetails(template.id)"
-      >
-        Details
-      </button>
+    <div class="flex flex-row justify-between items-center">
+      <span>{{ contract?.name }}</span>
+      <div class="flex space-x-2 mb-2">
+        <button
+          class="px-6 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition"
+          @click="openDetails(template.id)"
+        >
+          Details
+        </button>
+      </div>
+    </div>
+    <div class="flex flex-row justify-start items-center gap-3">
+      <ContractProgressIcon
+        class="!w-20 text-[#C9C9C9] !h-auto"
+        v-for="index in [1, 2, 3, 4]"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
+import ContractProgressIcon from "~/public/contract_progress.svg";
 const emits = defineEmits(["openModal", "openDetailsModal"]);
-const api = useApi();
-const store = useTemplateStore();
 
 const props = defineProps({
   contract: {
--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
     "@nuxtjs/color-mode",
+    "nuxt-svgo",
   ],
   imports: {
     dirs: [
