6b4c1991b14183c35bdf4541636eb3c6afd61a9a
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-10-31 19:18:20 +0200

    show contractItem component for each contract

---
+++ Frontend/components/contracts/ContractItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    :class="[
+      'p-4 border rounded-lg cursor-pointer transition flex items-center justify-between bg-white border-gray-300',
+    ]"
+  >
+    <span>{{ contract?.name }}</span>
+    <div class="flex space-x-2">
+      <button
+        class="px-6 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition"
+        @click="openDetails(template.id)"
+      >
+        Details
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emits = defineEmits(["openModal", "openDetailsModal"]);
+const api = useApi();
+const store = useTemplateStore();
+
+const props = defineProps({
+  contract: {
+    name: String,
+    id: Number,
+  },
+});
+
+const openDetails = async (id) => {
+  openDetailsModal();
+};
+
+const openModal = () => {
+  emits("openModal", true);
+};
+
+const openDetailsModal = () => {
+  emits("openDetailsModal", true);
+};
+</script>
--- Frontend/components/contracts/Contracts.vue
+++ Frontend/components/contracts/Contracts.vue
@@ -2,9 +2,11 @@
   <div class="mt-10 px-4 w-full max-w-5xl">
     <div class="space-y-4">
       <h1 class="text-3xl font-semibold my-6">My contracts</h1>
-      <div v-for="(template, index) in contractsStore.contracts" :key="index">
-        {{ template.name }}
-      </div>
+      <ContractItem
+        v-for="(contract, index) in contractsStore.contracts"
+        :key="index"
+        :contract="contract"
+      />
     </div>
   </div>
 </template>
@@ -12,6 +14,7 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { useContractsStore } from "@/stores/ContractsStore";
+import ContractItem from "./ContractItem.vue";
 
 const contractsStore = useContractsStore();
 
