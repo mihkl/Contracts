0a95801b1030e6fdb273c50e5543ca940061f3e0
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-11-01 14:27:48 +0200

    display contract status tooltip on svg hover

--- Frontend/components/contracts/ContractItem.vue
+++ Frontend/components/contracts/ContractItem.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    :class="[
-      'p-4 border rounded-lg cursor-pointer transition bg-white border-gray-300',
-    ]"
-  >
+  <div :class="['p-4 border rounded-lg transition bg-white border-gray-300']">
     <div class="flex flex-row justify-between items-center">
       <span>{{ contract?.name }}</span>
       <div class="flex space-x-2 mb-2">
@@ -16,10 +12,13 @@
       </div>
     </div>
     <div class="flex flex-row justify-start items-center gap-3">
-      <ContractProgressIcon
-        class="!w-20 text-[#C9C9C9] !h-auto"
-        v-for="index in [1, 2, 3, 4]"
-      />
+      <UTooltip
+        :text="contractStatuses[index]"
+        v-for="index in [0, 1, 2, 3]"
+        :popper="{ placement: 'top' }"
+      >
+        <ContractProgressIcon class="!w-20 text-[#C9C9C9] !h-auto" />
+      </UTooltip>
     </div>
   </div>
 </template>
@@ -28,7 +27,12 @@
 import ContractProgressIcon from "~/public/contract_progress.svg";
 const emits = defineEmits(["openModal", "openDetailsModal"]);
 
-const contractStatuses = ["LinkGenerated", "ContractSigned", "ContractCounterSigned", "FinalContractSentToApplicant"]
+const contractStatuses = [
+  "Link Generated",
+  "Contract Signed",
+  "Contract Countersigned",
+  "Final Contract Sent To Applicant",
+];
 
 const props = defineProps({
   contract: {
