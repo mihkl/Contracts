dc30644953a694fe5c2e95386ca274c9d3b8a66b
kevin <kekoos@ttu.ee>
2024-12-01 16:54:41 +0200

    Dark mode fix on Final Contract

--- Frontend/components/finalContracts/finalContractItem.vue
+++ Frontend/components/finalContracts/finalContractItem.vue
@@ -1,13 +1,15 @@
 <template>
-  <div :class="['p-4 border rounded-lg transition bg-white border-gray-300']">
+  <div :class="['p-4 border rounded-lg transition border-gray-300']">
     <div class="flex flex-row justify-between items-center">
       <span>{{ contract?.name }}</span>
-      <UButton
-        class="px-6 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition ml-auto"
+      <UButton 
+        class="px-6 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition ml-auto dark:bg-indigo-600 dark:hover:bg-indigo-700"
         @click="downloadSignedContract"
-      >
-        Download
+        color="indigo"
+        >
+          Download
       </UButton>
+
     </div>
   </div>
 </template>
