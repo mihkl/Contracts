f57db0bb9a3feb740110964011cfcbdce1ee7a5a
Martin Väli <martin.vali@tudengisatelliit.ee>
2024-12-06 17:17:00 +0200

    SMTP and email sending progress

--- Backend/API/emails/EmailsService.cs
+++ Backend/API/emails/EmailsService.cs
@@ -1,14 +1,31 @@
+using API.Data.Repos;
+using MailKit.Net.Smtp;
+
 namespace API.emails
 {
     public interface IEmailsService
     {
-        Task SendEmailsAsync(List<EmailMessage> emailMessages);
+        Task SendEmailsAsync(List<EmailMessage> emailMessages, string userId);
     }
-    public class EmailsService : IEmailsService
+    public class EmailsService(ISettingsRepo settingsRepo) : IEmailsService
     {
-        public Task SendEmailsAsync(List<EmailMessage> emailMessages)
+        private readonly ISettingsRepo _settingsRepo = settingsRepo;
+        public async Task SendEmailsAsync(List<EmailMessage> emailMessages, string userId)
         {
-            throw new NotImplementedException();
+            var smtpClient = await GetSmtpClient(userId);
+        }
+
+        private async Task<SmtpClient> GetSmtpClient(string userId)
+        {
+            var smtpClient = new SmtpClient();
+
+            var smtpSettings = await _settingsRepo.GetSmtpSettings(userId);
+            return smtpClient;
+        }
+
+        private async Task DisconnectSmtpClient(SmtpClient client)
+        {
+            await client.DisconnectAsync(true);
         }
     }
 }
